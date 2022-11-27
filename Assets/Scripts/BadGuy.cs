8ea25c7b7322d2643765e305061c4a9a89490a16
Tudor73 <tudorminciunescu@gmail.com>
2022-11-27 22:37:57 +0200

    #4 Make enemy follow path

--- Assets/Scripts/BadGuy.cs
+++ Assets/Scripts/BadGuy.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class BadGuy : MonoBehaviour
 {
+    private int wayPointIndex = 0;
+    private Transform target;
     public float speed = 0.3f;
-
     private Vector3 towerCenter = new Vector3(0f, 0f, 0f);
 
     private Transform currPos;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-
+        target = WayPoints.points[0];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.5f)
+        {
+            GetNextWayPoint();
+        }
     }
-    void FixedUpdate()
+    void GetNextWayPoint()
     {
-        Vector3 dir = towerCenter - transform.position;
-        this.transform.GetComponent<Rigidbody>().AddForce(dir * speed);
-        // rb.MovePosition(Vector3.zero);
+        if (wayPointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wayPointIndex++;
+        target = WayPoints.points[wayPointIndex];
     }
+    // void FixedUpdate()
+    // {
+    //     Vector3 dir = towerCenter - transform.position;
+    //     this.transform.GetComponent<Rigidbody>().AddForce(dir * speed);
+    //     // rb.MovePosition(Vector3.zero);
+    // }
 }
