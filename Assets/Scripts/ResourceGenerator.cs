dcb18c4267443cfdbb0a9316e9d3ab428d277d06
onumirea <oana.mirea@s.unibuc.ro>
2023-01-11 16:18:59 +0200

    kinda fixed chest spawn(not done yet)

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -24,7 +24,7 @@ public class ResourceGenerator : MonoBehaviour
 
     void SpawnChest()
     {
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-60, -10), 2, Random.Range(-45, -40));
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-70, -10), 2, Random.Range(-45, 40));
         Instantiate(chestPrefab, randomSpawnPosition, Quaternion.Euler(-90,0,0));
     }
 }
